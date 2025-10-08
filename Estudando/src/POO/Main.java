e8e5e0d30a067c2d59377bf0de9b387a95ed69b6
Kaik Lopes <kaiklopes2208@gmail.com>
2025-10-08 17:24:59 -0300

    Coreções sobre o codigo

--- Estudando/src/POO/Main.java
+++ Estudando/src/POO/Main.java
@@ -7,6 +7,8 @@ import java.time.format.DateTimeFormatter;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        // 1. Crie o formatador para ser reutilizado
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
         
         Aluno joao = new Aluno();
 
@@ -14,23 +16,25 @@ public class Main {
         joao.setName(sc.nextLine());
 
         System.out.println("Informe sua matricula: ");
-        joao.setMatricula(sc.nextLong());
+        joao.setMatricula(Long.parseLong(sc.nextLine())); // Boa prática: Evita problemas com o Scanner
 
-        System.out.println("Informe sua data de nascimento: ");
-        joao.setDataNascimento(LocalDate.parse(sc.next(), DateTimeFormatter.ofPattern("dd/MM/yyyy")));
+        System.out.println("Informe sua data de nascimento (dd/mm/aaaa): ");
+        joao.setDataNascimento(LocalDate.parse(sc.nextLine(), formatter));
 
         System.out.println("Informe sua turma: ");
-        joao.setTurma(sc.next());
+        joao.setTurma(sc.nextLine());
 
-        System.out.println("Informe seu sexo: ");
-        joao.setSexo(sc.next().charAt(0));
+        System.out.println("Informe seu sexo (M/F): ");
+        joao.setSexo(sc.nextLine().charAt(0));
 
+        System.out.println("\n--- Dados do Aluno ---");
         System.out.println("Nome: " + joao.getName());
         System.out.println("Matricula: " + joao.getMatricula());
-        System.out.println("Data de nascimento: " + joao.getDataNascimento());
+        
+        System.out.println("Data de nascimento: " + joao.getDataNascimento().format(formatter));
         System.out.println("Turma: " + joao.getTurma());
         System.out.println("Sexo: " + joao.getSexo());  
-       
-        }
-    
+        
+        sc.close();
+    }
 }
